3b0244ad9d22524dcc716cc8539902459a70a1d1
H-ERIKGM <erikyeudiel@hotmail.com>
2024-10-19 02:30:03 -0600

    Se agrega

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv'
-dotenv.config();
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
@@ -12,13 +15,17 @@ import counselingRoutes from './routes/counseling.routes.js'
 import classroomRoutes from './routes/classroom.routes.js'
 import registerRoutes from './routes/register.routes.js'
 
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:5173',
 }));
 app.use(morgan('dev'));
-app.use(express.json().static(path.join(__dirname, 'client/build')));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
