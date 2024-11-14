8007bd5cd87c275bb1a0cf29dd398aa182688406
H-ERIKGM <erikyeudiel@hotmail.com>
2024-11-14 08:09:14 -0600

    Se agrega

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
   return (
     <nav className="bg-red-950 my-3 flex justify-between py-5 px-10 rounded-lg">
       <h1 className="text-2xl font-bold">
-        <Link to={isAuthenticated ? "/classrooms" : "/"}>Salones</Link>
+        <Link to={isAuthenticated ? "/classrooms" : "/"}>AsesoraTEC</Link>
       </h1>
       <ul className="flex gap-x-2">
         {isAuthenticated ? (
--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client', 'dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'AW', 'client', 'dist', 'index.html'));
 });
 app.use(cookieParser());
 
