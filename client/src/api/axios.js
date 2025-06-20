301a56d15934c305451093a4c1a9f0ea434f0a49
H-ERIKGM <erikyeudiel@hotmail.com>
2025-06-20 06:08:24 -0600

    Se agrega

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://asesoratec.onrender.com',
+    baseURL: 'https://asesoratec.onrender.com/api',
     withCredentials: true
 })
 
